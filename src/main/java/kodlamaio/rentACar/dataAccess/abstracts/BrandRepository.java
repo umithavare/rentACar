bc58d858769a794122d106852aff47e19c0d084b
Umit Havare <umithavare16@gmail.com>
2023-08-20 17:28:32 +0300

    updated

--- src/main/java/kodlamaio/rentACar/dataAccess/abstracts/BrandRepository.java
+++ src/main/java/kodlamaio/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -1,12 +1,10 @@
 package kodlamaio.rentACar.dataAccess.abstracts;
 
 import kodlamaio.rentACar.entities.concretes.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
-public interface BrandRepository {
-    List<Brand> getAll();
-
+public interface BrandRepository  extends JpaRepository<Brand,Integer> {
 
 }
--- src/main/java/kodlamaio/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlamaio/rentACar/entities/concretes/Brand.java
@@ -1,30 +1,24 @@
 package kodlamaio.rentACar.entities.concretes;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id otomatik artması icim yazildi
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
 
-    public Brand() {
-    }
 
-    public Brand(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
